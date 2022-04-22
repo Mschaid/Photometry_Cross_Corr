corrected normalization function to control for most extreme value.

PSTH_cross_corr.py
@@ -1,16 +1,11 @@
 import numpy as np
 import pandas as pd
-import h5py
-import matplotlib.pyplot as plt
 import os
 import glob
 import cross_correlation_setup as cc
 import fnmatch
-import functools
-import operator
-import itertools
 from scipy import signal
-import sys
+
 
 
 path_to_folders = r"C:\Users\mds8301\Desktop\PSTH_corr_test"
@@ -39,29 +34,58 @@ for i in sub_folders:
     # and converting into np array for speed
     df_A = pd.read_hdf(f"{i}\\{signal_A}")
     df_A = df_A[df_A["timestamps"].between(filter_ts_low, filter_ts_high)]
+    df_A = df_A.rolling(window = 500, center=True).mean().dropna()
     A_arr = df_A.iloc[:, :-3].T.to_numpy()
 
     df_B = pd.read_hdf(f"{i}\\{signal_B}")
     df_B = df_B[df_B["timestamps"].between(filter_ts_low, filter_ts_high)]
-    B_arr = df_B.iloc[:, :-4].T.to_numpy()
+    df_B = df_B.rolling(window=500, center=True).mean().dropna()
+    B_arr = df_B.iloc[:, :-3].T.to_numpy()
 
     # create empty list to build array of correlations for each event
     cross_corr = []
     cross_corr *= 0  # ensures list is empty
-
     # zip signal A and signal B arrays to iterate in parallel  and calculate correlation coefficient
+    ##TO DO check weird values in console to make sure this is working. it shouldn't be giving values over 1?
     for (a, b) in zip(A_arr, B_arr):
         corr = signal.correlate(a, b)
-        corr /= np.max(corr)
-        cross_corr.append(corr)
+        corr_norm = cc.normalize(corr)
+        cross_corr.append(corr_norm)
         lag = signal.correlation_lags(len(a), len(b))
-        lag_msec = lag / 1.017e2
+        lag_msec = lag / 1.017e3
         cross_corr_arr = np.asarray(cross_corr, dtype="float16").transpose()
     # create new dataframe, calculate mean, sem and attach lag time
     df_c = pd.DataFrame(cross_corr_arr)
-    df_c = df_c.assign(mean=df_c.mean(axis=1), sem=df_c.sem(axis=1), lag_time=lag_msec)
+    df_c = df_c.assign(mean=df_c.mean(axis=1),
+                       sem=df_c.sem(axis=1),
+                       lag_time=lag_msec)
     #  save to csv in output file path + new folder we created
     df_c.to_csv(f'{i}\\{new_folder}\\{os.path.basename(i).split("_")[0]}_{signal_A.split(".")[0]}_VS_{signal_B.split(".")[0]}.csv')
+print('analysis completed and files saved')
 
+#%%
+day ='day12'
+files_by_day = f"{path_to_folders}\\{day}"
+
+compiled_files = []
+for dirpath, dirs, files in os.walk(files_by_day):
+  for filename in fnmatch.filter(files, f'*VS*.csv'):
+    compiled_files.append((os.path.join(dirpath, filename)))
+
+all_df = []
+for f in compiled_files:
+    df = pd.read_csv(f)
+    df = (df.rename(columns={
+        'mean': f"{(os.path.basename(f).split('_')[0])}_{day}_mean"})
+    )
+    all_df.append(df.iloc[:,-3])
+df_grp = pd.DataFrame(all_df).T
+df_grp = (df_grp.assign(group_mean=df_grp.mean(axis=1),
+              std = df_grp.std(axis=1),
+              sem=df_grp.sem(axis=1),
+              lag_time=lag_msec)
+          )
+path_to_save_grp = path_to_folders
+df_grp.to_csv(f"{path_to_folders}_NAc_VS_LHA_{day}")
 
 #%%

cross_correlation_setup.py
@@ -18,7 +18,9 @@ def new_folder(path, name):
 
 ##  function to compile list of files to analyze
 def get_data(path, search_for, file_type):
+
     list_of_files = []
+    list_of_files *= 0
     for path, currentDirectory, files in os.walk(path):
         for file in files:
             if file.startswith(search_for) and file.endswith(file_type):
@@ -37,7 +39,7 @@ def read_hdf5(file, input):
 
 ## function to get mouse ID from guppy output
 def get_id(path):
-    mouse_id = path.split("\\")[-2].split("-")[0]
+    mouse_id = path.split("\\")[-2].split("_")[0]
     return mouse_id
 
 
@@ -50,3 +52,11 @@ def df_cross_correlate(signal_A, signal_B):
     lag = signal.correlation_lags(len(signal_A), len(signal_B))
     lag_msec = (lag/1.017E2)
     return (corr, lag_msec)
+
+
+def normalize(array):
+    if np.max(array) >= -np.min(array):
+        arr = array/np.max(array)
+    else:
+        arr = array/(- np.min(array))
+    return arr