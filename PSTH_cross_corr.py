0c2751af8a01d66e4f562028453de7e3b936c89d
Mike_Schaid <68658862+Mschaid@users.noreply.github.com>
2022-04-22 17:09:18 -0500

    corrected normalization function 2.0 with np.max(np.abs(array)) instead of if/else function to control for most extreme value.
    added saving options for plots

--- PSTH_cross_corr.py
+++ PSTH_cross_corr.py
@@ -7,16 +7,16 @@ import fnmatch
 from scipy import signal
 
 
-
 path_to_folders = r"C:\Users\mds8301\Desktop\PSTH_corr_test"
 
-## search path for guppy outputs
+# search path for guppy outputs
 sub_folders = glob.glob(path_to_folders + "\\**\\*output_*", recursive=True)
 
-## create new file called cross corelation analysis
+# create new file called cross corelation analysis
 new_folder = "cross_correlation_analysis"
 
-## create new folder in each output folder called 'cross_correlation_analyis - indiv analysis will be stored here
+# create new folder in each output folder called
+# 'cross_correlation_analyis - indiv analysis will be stored here
 for i in sub_folders:
     cc.new_folder(path=i, name=new_folder)
 
@@ -25,31 +25,32 @@ signal_A = "cue_NAC_z_score_NAC.h5"
 signal_B = "cue_LH_z_score_LH.h5"
 
 # set parameters to filter around time 0
-filter_ts_low = -3
-filter_ts_high = 3
+filter_ts_low = -2
+filter_ts_high = 2
 
-## in each ouput get cue z_score
+# in each ouput get cue z_score
 for i in sub_folders:
     # here we are reading the hdf files into a dataframe, filterting epoch around time zero
     # and converting into np array for speed
     df_A = pd.read_hdf(f"{i}\\{signal_A}")
     df_A = df_A[df_A["timestamps"].between(filter_ts_low, filter_ts_high)]
-    df_A = df_A.rolling(window = 500, center=True).mean().dropna()
+    df_A = df_A.rolling(window=1000, center=True).mean().dropna()
     A_arr = df_A.iloc[:, :-3].T.to_numpy()
 
     df_B = pd.read_hdf(f"{i}\\{signal_B}")
     df_B = df_B[df_B["timestamps"].between(filter_ts_low, filter_ts_high)]
-    df_B = df_B.rolling(window=500, center=True).mean().dropna()
+    df_B = df_B.rolling(window=1000, center=True).mean().dropna()
     B_arr = df_B.iloc[:, :-3].T.to_numpy()
 
     # create empty list to build array of correlations for each event
     cross_corr = []
     cross_corr *= 0  # ensures list is empty
     # zip signal A and signal B arrays to iterate in parallel  and calculate correlation coefficient
-    ##TO DO check weird values in console to make sure this is working. it shouldn't be giving values over 1?
+    # TO DO check weird values in console to make sure this is working. it
+    # shouldn't be giving values over 1?
     for (a, b) in zip(A_arr, B_arr):
         corr = signal.correlate(a, b)
-        corr_norm = cc.normalize(corr)
+        corr_norm = corr/ np.max(np.abs(corr))
         cross_corr.append(corr_norm)
         lag = signal.correlation_lags(len(a), len(b))
         lag_msec = lag / 1.017e3
@@ -60,17 +61,18 @@ for i in sub_folders:
                        sem=df_c.sem(axis=1),
                        lag_time=lag_msec)
     #  save to csv in output file path + new folder we created
-    df_c.to_csv(f'{i}\\{new_folder}\\{os.path.basename(i).split("_")[0]}_{signal_A.split(".")[0]}_VS_{signal_B.split(".")[0]}.csv')
+    df_c.to_csv(
+        f'{i}\\{new_folder}\\{os.path.basename(i).split("_")[0]}_{signal_A.split(".")[0]}_VS_{signal_B.split(".")[0]}.csv')
 print('analysis completed and files saved')
 
-#%%
-day ='day12'
+# %%
+day = 'day12'
 files_by_day = f"{path_to_folders}\\{day}"
 
 compiled_files = []
 for dirpath, dirs, files in os.walk(files_by_day):
-  for filename in fnmatch.filter(files, f'*VS*.csv'):
-    compiled_files.append((os.path.join(dirpath, filename)))
+    for filename in fnmatch.filter(files, f'*VS*.csv'):
+        compiled_files.append((os.path.join(dirpath, filename)))
 
 all_df = []
 for f in compiled_files:
@@ -78,14 +80,15 @@ for f in compiled_files:
     df = (df.rename(columns={
         'mean': f"{(os.path.basename(f).split('_')[0])}_{day}_mean"})
     )
-    all_df.append(df.iloc[:,-3])
+    all_df.append(df.iloc[:, -3])
 df_grp = pd.DataFrame(all_df).T
 df_grp = (df_grp.assign(group_mean=df_grp.mean(axis=1),
-              std = df_grp.std(axis=1),
-              sem=df_grp.sem(axis=1),
-              lag_time=lag_msec)
+                        std=df_grp.std(axis=1),
+                        sem=df_grp.sem(axis=1),
+                        lag_time=lag_msec)
           )
 path_to_save_grp = path_to_folders
-df_grp.to_csv(f"{path_to_folders}_NAc_VS_LHA_{day}")
+df_grp.to_csv(
+    r"C:\Users\mds8301\Desktop\PSTH_corr_test\\_NAc_VS_LHA_day12.csv")
 
-#%%
+# %%
--- plot_cross_corr.py
+++ plot_cross_corr.py
@@ -3,15 +3,47 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_hdf(r'R:\Mike\JS_for_MS\cross_correlation_analysis_compiled_.h5').reset_index(drop=True)
-data = data.iloc[::5,:]
-
-sns.lineplot(data=data, x='lag_time(ms)', y='co_coef', hue = 'correlation', ci=0.68, err_style='band')
-plt.show()
-
+data = pd.read_csv(r"C:\Users\mds8301\Desktop\PSTH_corr_test\_NAc_VS_LHA_day12.csv")
 print(data)
 
 
+
 #%%
+axs_list= [0]
+fig, axs = plt.subplots(ncols = 2,
+                       nrows = 1,
+                        squeeze=True)
+fig.suptitle('NAc VS LH \n'
+             'day12')
+sns.lineplot(data= data, x= 'lag_time', y= 'group_mean', ax= axs[0], )
+axs[0].fill_between(data['lag_time'],
+                    data['group_mean']+ data['sem'],
+                    data['group_mean']- data['sem'],
+                    alpha=0.3,
+                    linewidth= 0)
+
+
+sns.heatmap(data= data.iloc[:,1:-4].T,
+            cbar=False,
+            yticklabels=False,
+            cmap= 'Blues',
+            ax = axs[1]
+            )
+axs[1].set_xticks([])
+for a in axs_list:
+    #  x axis parameters
+    axs[a].tick_params(axis='both', which='major', labelsize=12)
+    axs[a].set_xlabel('Lag time(sec)',fontsize = 12, labelpad=2) #    x axis label
+    axs[a].set_ylabel('Correlation Coefficient', fontsize=12, labelpad=2)
+    sns.despine(ax=axs[a])
+plt.tight_layout()
+plt.rcParams['svg.fonttype'] = 'none' #save text as text in svg
+plt.savefig(r"C:\Users\mds8301\Desktop\PSTH_corr_test\_NAc_VS_LHA_day12.tiff",
+            dpi=300,
+            transparent=True)
+plt.savefig(r"C:\Users\mds8301\Desktop\PSTH_corr_test\_NAc_VS_LHA_day12.svg",
+            dpi=300,
+            transparent=True)
+plt.show()
 
 
